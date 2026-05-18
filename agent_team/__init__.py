299120b986294179202bb1bd5601861ad32ee0e7
Chinmay Panda <chinmay@openautonomyx.com>
2026-05-18 08:48:32 -0700

    Add Agent-Team runtime package

---
+++ agent_team/__init__.py
@@ -0,0 +1,5 @@
+"""Reusable goal-oriented agent teams for AGenNext products."""
+
+from agent_team.registry import AgentRegistry, build_bootstrapped_team_registry
+
+__all__ = ["AgentRegistry", "build_bootstrapped_team_registry"]
